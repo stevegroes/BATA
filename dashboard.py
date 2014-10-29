f7e7bad1525db84fb634aa6b6f31f626de59cd83
rogier2012 <r.monshouwer@student.utwente.nl>
2014-10-29 12:06:52 +0100

    Daar is de dashboard
    zeker

---
+++ dashboard.py
@@ -0,0 +1,31 @@
+from eca import *
+from eca.generators import start_offline_tweets
+
+import random
+import datetime
+import textwrap
+import pprint
+import re
+
+## You might have to update the root path to point to the correct path
+## (by default, it points to <rules>_static)
+# root_content_path = 'template_static'
+
+
+# binds the 'setup' function as the action for the 'init' event
+# the action will be called with the context and the event
+@event('init')
+def setup(ctx, e):
+    ctx.count = 0
+    fire('tweet', start_offline_tweets('data/bata_2014.txt', time_factor=100000))
+	
+stopwords = ['hup']
+@event('tweet')
+def echo(c,e):
+	tweet = e.data
+	result = []
+	result = filter(lambda w: w in stopwords, result)
+	emit ('tweet', tweet)
+
+
+	
